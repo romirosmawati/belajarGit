638d0cc02ab30dfb526ba31846782c5d81dbdd3b
Romi R <32626103+romirosmawati@users.noreply.github.com>
2024-06-05 19:05:28 +0700

    update hello world
    update hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class Test
 { 
     public static void main(String []args) 
     { 
-        System.out.println("Program Java Pertama Saya."); 
+        System.out.println("Hello World. Program Java Pertama Saya."); 
     } 
 };
